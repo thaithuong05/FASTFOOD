5ad97a3ddf27d135ad22a425184644dd29db8dfd
Thái Thưởng <thaithuong09021997@icloud.com>
2024-11-26 14:49:00 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,22 +1,22 @@
 // Ví dụ về chức năng quản lý người dùng
 function manageUsers() {
   // Các hàm quản lý người dùng sẽ được thêm vào đây
-  console.log("Quản lý người dùng");
+  alert("Quản lý người dùng");
 }
 
 //Tài khoản admin
-const loginForm = document.getElementById('loginForm');
+const loginForm = document.getElementById('admin-loginForm');
 const open_admin = document.getElementById('open-admin');
 const display_account_admin = document.getElementById('display-account-admin');
 
 // Kiểm tra đăng nhập
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const admin_username = document.getElementById('admin-username').value;
+  const admin_password = document.getElementById('admin-password').value;
 
   // Kiểm tra thông tin đăng nhập (có thể thay đổi theo yêu cầu)
-  if (username === 'thi' && password === '123') {
+  if (admin_username === '0987654321' && admin_password === '123456') {
     open_admin.style.display = 'block';
     loginForm.style.display = 'none';
     display_account_admin.style.display = 'none';
